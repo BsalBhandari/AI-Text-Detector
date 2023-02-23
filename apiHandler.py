0a8a6a53cab568de0fe357621920306742aba94e
bsalbhandari <bsalvandarii075@gmail.com>
2023-02-23 11:54:10 -0500

    data extraction from api

--- apiHandler.py
+++ apiHandler.py
@@ -32,17 +32,55 @@ api = ApiHandler()
 def index():
     return render_template('index.html')
 
-
-# this route handles the data from the user and if the response data is not none it return another html page result.html and the passes the result
+'''
+this route handles the data from the user and if the response data is not none it render another html page result.html
+it also passes arguments that contain the data from the api and the other three data that should be seen by users
+which is avg_perplexity_score, highest_perplexity_score and finally the burstiness_score
+'''
 @app.route('/process_form', methods=['POST'])
 def process_form():
     document = request.form['document']
     payload = {'document': document}
     response_data = api.send_data(payload)
     if response_data is not None:
-        return render_template('result.html', result=response_data)
+        avg_perplexity_score, perplexity_score, burstiness_score = extract_scores(response_data)
+        return render_template(
+            'result.html',
+            result=response_data, 
+            avg_perplexity_score=avg_perplexity_score, 
+            highest_perplexity_score=perplexity_score, 
+            burstiness_score=burstiness_score
+            )
     else:
         return "Failed to send data"
+
+
+def calculate_avg_perplexity(data):
+    total_perplexity = 0
+    total_sentences = 0
+    for document in data:
+        sentences = document['sentences']
+        for sentence in sentences:
+            perplexity = sentence['perplexity']
+            total_perplexity += perplexity
+            total_sentences += 1
+    if total_sentences > 0:
+        return total_perplexity / total_sentences
+    else:
+        return 0
+
+
+def get_highest_perplexity(data):
+    perplexities = [sentence['perplexity']
+                    for sentence in data[0]['sentences']]
+    return max(perplexities)
+
+
+def extract_scores(data):
+    avg_perplexity_score = calculate_avg_perplexity(data)
+    perplexity_score = get_highest_perplexity(data)
+    burstiness_score = data[0]['overall_burstiness']
+    return avg_perplexity_score, perplexity_score, burstiness_score
 
 
 if __name__ == '__main__':
