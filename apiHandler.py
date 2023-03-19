4efae65f78eef5c61d9980a7905510a54f147c6e
Nishar Miya <103556082+miyannishar@users.noreply.github.com>
2023-03-19 18:00:50 -0400

    update apihandler

--- apiHandler.py
+++ apiHandler.py
@@ -1,7 +1,8 @@
+from flask import Flask, render_template
 from flask import Flask, render_template, request
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 class ApiHandler:
@@ -25,32 +26,45 @@ class ApiHandler:
 
 api = ApiHandler()
 
-## index is the first webpage that should be render in this webpage
+# index is the first webpage that should be render in this webpage
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 '''
 this route handles the data from the user and if the response data is not none it render another html page result.html
 it also passes arguments that contain the data from the api and the other three data that should be seen by users
 which is avg_perplexity_score, highest_perplexity_score and finally the burstiness_score
 '''
+
+
+# @app.route('/process_form', methods=['POST'])
 @app.route('/process_form', methods=['POST'])
 def process_form():
-    document = request.form['document']
+    document = request.form['parameter']
+    # return document
     payload = {'document': document}
+    # return payload
     response_data = api.send_data(payload)
+    return response_data
+    # if response_data is not None:
+
+    #     return response_data
+    # else:
+    #     return "Failed to send dat"
     if response_data is not None:
-        avg_perplexity_score, perplexity_score, burstiness_score = extract_scores(response_data)
+        avg_perplexity_score, perplexity_score, burstiness_score = extract_scores(
+            response_data)
         return render_template(
-            'result.html',
-            result=response_data, 
-            avg_perplexity_score=avg_perplexity_score, 
-            highest_perplexity_score=perplexity_score, 
+            'templates/index.html',
+            result=response_data,
+            avg_perplexity_score=avg_perplexity_score,
+            highest_perplexity_score=perplexity_score,
             burstiness_score=burstiness_score
-            )
+        )
     else:
         return "Failed to send data"
 
@@ -83,5 +97,13 @@ def extract_scores(data):
     return avg_perplexity_score, perplexity_score, burstiness_score
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def homepage():
+#     return render_template("index.html")
